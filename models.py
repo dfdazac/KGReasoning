35340e1ab806031cad9c30898e847e7707862380
Daniel <dfdazac@gmail.com>
2021-01-13 18:29:20 +0100

    Add final scoring step for 1-hop and conjunctive queries

--- models.py
+++ models.py
@@ -786,34 +786,37 @@ class CQD(nn.Module):
 
     def forward(self, positive_sample, negative_sample, subsampling_weight, batch_queries_dict, batch_idxs_dict):
         all_idxs = []
+        all_scores = []
 
         for query_structure, queries in batch_queries_dict.items():
             batch_size = queries.shape[0]
             atoms, num_variables = query_to_atoms(query_structure, queries)
             all_idxs.extend(batch_idxs_dict[query_structure])
 
-            if num_variables > 1:
-                # Offsets identify variables across different batches
-                var_id_offsets = torch.arange(batch_size, device=atoms.device) * num_variables
-                var_id_offsets = var_id_offsets.reshape(-1, 1, 1)
+            target_mask = torch.sum(atoms == -num_variables, dim=-1) > 0
 
+            # Offsets identify variables across different batches
+            var_id_offsets = torch.arange(batch_size, device=atoms.device) * num_variables
+            var_id_offsets = var_id_offsets.reshape(-1, 1, 1)
+
+            # Replace negative variable IDs with valid identifiers
+            vars_mask = atoms < 0
+            atoms_offset_vars = -atoms + var_id_offsets
+            atoms[vars_mask] = atoms_offset_vars[vars_mask]
+
+            head, rel, tail = atoms[..., 0], atoms[..., 1], atoms[..., 2]
+            head_vars_mask = vars_mask[..., 0]
+
+            with torch.no_grad():
+                h_emb_constants = self.embeddings[0](head)
+                r_emb = self.embeddings[1](rel)
+
+            if num_variables > 1:
                 # var embedding for ID 0 is unused for ease of implementation
                 var_embs = nn.Embedding((num_variables * batch_size) + 1,
                                         self.rank * 2)
                 var_embs.to(atoms.device)
                 optimizer = optim.Adam(var_embs.parameters(), lr=0.1)
-
-                # Replace negative variable IDs with valid identifiers
-                vars_mask = atoms < 0
-                atoms_offset_vars = -atoms + var_id_offsets
-                atoms[vars_mask] = atoms_offset_vars[vars_mask]
-
-                head, rel, tail = atoms[..., 0], atoms[..., 1], atoms[..., 2]
-                head_vars_mask = vars_mask[..., 0]
-
-                with torch.no_grad():
-                    h_emb_constants = self.embeddings[0](head)
-                    r_emb = self.embeddings[1](rel)
 
                 # CQD-CO optimization loop
                 for i in range(100):
@@ -831,8 +834,23 @@ class CQD(nn.Module):
                     optimizer.zero_grad()
                     loss.backward()
                     optimizer.step()
+            else:
+                h_emb = h_emb_constants
 
-        # TODO: score
-        scores = torch.zeros(negative_sample.shape, device=queries.device)
+            with torch.no_grad():
+                # Select predicates involving target variable only
+                target_mask = target_mask.unsqueeze(-1).expand_as(h_emb)
+                emb_size = h_emb.shape[-1]
+                h_emb = h_emb[target_mask].reshape(batch_size, -1, 1, emb_size)
+                r_emb = r_emb[target_mask].reshape(batch_size, -1, 1, emb_size)
+                to_score = self.embeddings[0].weight
+
+                scores, factors = self.score_emb(h_emb, r_emb, to_score)
+                scores = torch.sigmoid(scores)
+                t_norm = torch.prod(scores, dim=1)
+
+                all_scores.append(t_norm)
+
+        scores = torch.cat(all_scores, dim=0)
 
         return None, scores, None, all_idxs
